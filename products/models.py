346fa092ee69997f642687e7c0ceefeeec6bf93c
soheil rasekh <soheilrasekh555@gmail.com>
2020-08-01 16:31:30 +0430

    create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title           = models.CharField(max_length=255)
+    detail          = models.TextField()
+    price           = models.DecimalField(decimal_places=2,max_digits=5)
+
+    #TODO: implement images and slug fields
+
+    #images          = models.ImageField()
+    #slug            = models.SlugField()
+
+    def __str__(self):
+        return self.title
